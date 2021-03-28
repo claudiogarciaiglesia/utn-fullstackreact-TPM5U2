9a4fcfe9d4eccf2c28fe9918b9f01d00103aea94
Claudio Garcia Iglesia <claudiogarciaiglesia@gmail.com>
2021-03-27 22:14:19 -0300

    fix description overflow

--- src/Description.jsx
+++ src/Description.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Description = (props) => {
     return (
         <div className="componente-descripcion">
-            {props.desc}
+            <span> {props.desc}</span>
         </div>
     )
 }
--- src/datos.js
+++ src/datos.js
@@ -14,6 +14,10 @@ const vector = [
     {
         url: 'https://placeimg.com/80/80/people?id=4', title: 'Titulo 4', description:
             'Descripcion imagen 4'
+    },
+    {
+        url: 'https://placeimg.com/80/80/people?id=5', title: 'Titulo 5', description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
 ]
 
